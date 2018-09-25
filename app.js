18ccdb35b90c15310da5418f3bb7a3172da955e3
SINZED <fastrongs@gmail.com>
2018-09-25 10:52:32 +0330

    'test2'

--- app.js
+++ app.js
@@ -1,23 +1,30 @@
-const request = require('request');
-const yargs = require('yargs');
-const getYear = require('./year');
-const argv = yargs
-  .options({
-    n: {
-      demand: true,
-      alias: 'name',
-      describe: 'Title of film',
-      string: true
-    }
-  })
-  .help()
-  .alias('help','h')
-  .argv;
-
-getYear.getFunc(argv,(error , response) => {
-  if(error){
-    console.log(error);
-  }else{
-    console.log(JSON.stringify(response , undefined , 2));
-  }
-}); 
+// const request = require('request');
+// const yargs = require('yargs');
+// const getYear = require('./year');
+// const argv = yargs
+//   .options({
+//     n: {
+//       demand: true,
+//       alias: 'name',
+//       describe: 'Title of film',
+//       string: true
+//     }
+//   })
+//   .help()
+//   .alias('help','h')
+//   .argv;
+//
+// getYear.getFunc(argv,(error , response) => {
+//   if(error){
+//     console.log(error);
+//   }else{
+//     console.log(JSON.stringify(response , undefined , 2));
+//   }
+// });
+const express = require('express')
+const port = process.env.port || 3000;
+var app = express();
+app.get('/',(req,res) => {
+  res.send('Hello Express!');
+})
+app.listen(port);
